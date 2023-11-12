03926b0bc2b470b8dc8d71559f3188c7583d6b71
devow <dev.diogo@proton.me>
2023-11-11 23:39:27 -0300

    added main game loop

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 
 // Jogo do Herói
 
-// 'npm prompt-sync' é uma dependência para código funcionar em node.js
+// 'npm prompt-sync' habilita prompts em node.js
 const prompt = require('prompt-sync')();
 
 // Variáveis básicas
 let nomeHeroi = prompt("Escolha um nome para o Herói: ", "Padrão");
-let xpHeroi = 0
+let xp = 0
 let nivel = "Ignorante"
 
-let mensagem = console.log("O Herói de nome "+ nomeHeroi + " está no nível de " + nivel + ".")
+// Informações do Herói
+let mensagem = "O Herói de nome "+ nomeHeroi + " está no nível "
 
-if (nomeHeroi != null) {
-    mensagem
-    return
+// Loop do jogo
+
+while (xp <= 11000) {
+    
+    // Incrementa a xp em 1 a cada nova iteração do loop
+    xp+= 1
+
+    // Condicionais que definem o nivel do Herói
+    if (xp <= 1000) {nivel = "Ferro"};
+    if (xp >= 1001) {nivel = "Bronze"};
+    if (xp >= 2001) {nivel = "Prata"};
+    if (xp >= 5001) {nivel = "Ouro"};
+    if (xp >= 7001) {nivel = "Platina"};
+    if (xp >= 8001) {nivel = "Ascendente"};
+    if (xp >= 9001) {nivel = "Imortal"};
+    if (xp >= 10001) {nivel = "Radiante"};
+
+    // Garante que apenas as mensagens de troca de nível sejam exibidas
+    switch (xp) {
+        case 1: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 1001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 2001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 5001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 5001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 7001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 8001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 9001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+        case 10001: console.log(mensagem + nivel + ", " + xp + "xp.")
+        break;
+    }
 }
