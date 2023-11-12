87e4cec818e9aad64c94ca51d57891f2b0f46321
devow <dev.diogo@proton.me>
2023-11-11 21:13:58 -0300

    add variables

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+// Jogo do Herói
+
+let nomeHeroi = "Padrao"
+let xpHeroi = 0
+let nivel = "Ignorante"
+
+let mensagem = console.log("O Herói de nome "+ nomeHeroi + " está no nível de " + nivel + ".")
+
+mensagem
