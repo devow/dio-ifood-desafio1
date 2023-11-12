225fb88daffbd5eb26c2af739ddc89a7b6a3d198
devow <dev.diogo@proton.me>
2023-11-11 21:32:27 -0300

    added npm prompt sync for inputs

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-
 // Jogo do Herói
 
-let nomeHeroi = "Padrao"
+// 'npm prompt-sync' é uma dependência para código funcionar em node.js
+const prompt = require('prompt-sync')();
+
+// Variáveis básicas
+let nomeHeroi = prompt("Escolha um nome para o Herói: ", "Padrão");
 let xpHeroi = 0
 let nivel = "Ignorante"
 
 let mensagem = console.log("O Herói de nome "+ nomeHeroi + " está no nível de " + nivel + ".")
 
-mensagem
+if (nomeHeroi != null) {
+    mensagem
+    return
+}
